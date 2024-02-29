92388130ab24d8d025d0ea289241d1aafec6ce2a
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2024-02-29 02:15:53 +0100

    test_newCentrality_v3_forNicolas.py : PEP8 compliance

--- Interactome/test_newCentrality_v3_forNicolas.py
+++ Interactome/test_newCentrality_v3_forNicolas.py
@@ -10,13 +10,14 @@ import seaborn
 
 import tqdm
 
+
 def causal_genes_at_distance(dict_distances, node, d):
     '''
     Calculates the number of causal genes from a node of interest at the given distance d.
 
     Arguments:
-        - dict_distances: dictionary with structure 
-                            {node (non-causal): 
+        - dict_distances: dictionary with structure
+                            {node (non-causal):
                                 {node (causal): distance
                                 ...}
                             ...}
@@ -28,9 +29,10 @@ def causal_genes_at_distance(dict_distances, node, d):
     '''
     try:
         return len([dist for dist in dict_distances.get(node).values() if dist == d])
-    except:
+    except Exception:
         return 0
-    
+
+
 # set phenotype name to choose from ["MMAF", "NOA", "OG"], type=str
 PHENOTYPE = "MMAF"
 
@@ -41,8 +43,8 @@ ALPHA = 0.5
 Step 1. Load data
 '''
 # load an interactome .tsv file generated with grexome-TIMC-interactome to 2D numpy array of size=(number of interactions, 3), dtype=str
-# each row in the .tsv file should be: 
-#  gene1  gene2   1 
+# each row in the .tsv file should be:
+#  gene1  gene2   1
 interactome = numpy.genfromtxt("./data/Interactome_human.tsv", delimiter='\t', dtype=str)
 
 # initialize networkx graph object
@@ -50,7 +52,7 @@ G = networkx.Graph()
 
 # iterate over rows of the interactome numpy array
 for row in interactome:
-    
+
     # unpack row values (gene1, gene2, 1) to variables
     gene1, gene2, interaction = row
 
@@ -65,7 +67,7 @@ causal_genes = numpy.load(f"./data/causalGenesList_{PHENOTYPE}.p", allow_pickle=
 causal_genes = list(set([c for c in causal_genes if c in G.nodes()]))
 
 # create a list of size=(number of non-causal genes) to store non-causal gene names, dtype=str
-nonCausal_genes = [n for n in G.nodes() if n not in causal_genes] 
+nonCausal_genes = [n for n in G.nodes() if n not in causal_genes]
 
 # print interactome information (number of nodes, number of edges, number of causal genes)
 print(f"Interactome size: {G.number_of_nodes()} nodes, {G.number_of_edges()}, number of {PHENOTYPE} candidates in interactome: {len(causal_genes)}")
@@ -90,9 +92,9 @@ for source_node in tqdm.tqdm(nonCausal_genes):
             distance = networkx.shortest_path_length(G, source_node, target_node)
             dict_tmp[target_node] = distance
         # if path doesn't exist, skip the iteration
-        except:
+        except Exception:
             continue
-    
+
     # save the distances between all causal genes to the current non-causal gene in the dictionary
     dict_distances[source_node] = dict_tmp
 
@@ -123,7 +125,7 @@ res.setdiag(0)
 res_sparse = scipy.sparse.csr_matrix(res, dtype=int)
 # save sparse A^2 to the dictionary
 dict_adjacency[2] = res_sparse
- 
+
 # calculate A to the powers of 3 to 4 with shape=(number of nodes, number of nodes), dtype=int64
 # tqdm.tqdm prints a progress bar of loop iteration on the screen
 for power in tqdm.tqdm(range(3, 5)):
@@ -152,17 +154,16 @@ scores_normalized = numpy.zeros((len(causal_genes_array)))
 # iterate over distances 1 to 4
 for d in range(1, 5):
     # get the adjacency matrix A^distance from dictionary
-    A = dict_adjacency.get(d) 
-    
+    A = dict_adjacency.get(d)
+
     # calculate score for each node i in the following way:
     # score_i = sum_over_distances ( ALPHA^distance * ( sum_over_nonCausal_genes (A^distance)_ij * score_j ) )
-    scores += ALPHA ** d * A.dot(causal_genes_array) 
-
+    scores += ALPHA ** d * A.dot(causal_genes_array)
 
     # calculate normalization factor for each node i in the following way:
     # norm_factor_i = sum_over_distances ( ALPHA^distance * ( sum_over_all_genes_j (A^distance)_ij ) )
     for i in range(len(norm_factors)):
-        norm_factors[i] += ALPHA ** d * A[i, :].sum() 
+        norm_factors[i] += ALPHA ** d * A[i, :].sum()
 
 # normalize score for each node i: score_normalized_i = score_i / normalization_factor_i
 # note: to avoid division by zero, if normalization_factor_i=0, do as if normalization_factor_i=1
@@ -192,16 +193,16 @@ for node, score in dict_scores_sorted.items():
     # score, node degree, causal genes at various distances (1 to 4)
     dict_final[node] = [round(score, 4),
                         G.degree(node),
-                        causal_genes_at_distance(dict_distances, node, 1), 
+                        causal_genes_at_distance(dict_distances, node, 1),
                         causal_genes_at_distance(dict_distances, node, 2),
                         causal_genes_at_distance(dict_distances, node, 3),
                         causal_genes_at_distance(dict_distances, node, 4)]
 
 # to represent results in an easily readable way, create a pandas dataframe with columns:
 # ['score', 'degree', 'causal genes at d=1', 'causal genes at d=2', 'causal genes at d=3', 'causal genes at d=4']
-df = pandas.DataFrame.from_dict(dict_final, 
-                            orient='index', 
-                            columns=['score', 'degree', 'causal genes at d=1', 'causal genes at d=2', 'causal genes at d=3', 'causal genes at d=4'])
+df = pandas.DataFrame.from_dict(dict_final,
+                                orient='index',
+                                columns=['score', 'degree', 'causal genes at d=1', 'causal genes at d=2', 'causal genes at d=3', 'causal genes at d=4'])
 
 # print top (highest-scoring) 20 non-causal genes
 print(df.head(20))
