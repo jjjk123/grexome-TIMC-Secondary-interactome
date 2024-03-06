6c9f58590400aa16cb80262b024a14f81c9e6cf8
jjjk123 <jj.kubica@student.uw.edu.pl>
2024-03-06 16:42:40 +0100

    upd to use utils

--- Interactome/degree_centrality.py
+++ Interactome/degree_centrality.py
@@ -7,49 +7,10 @@ import pathlib
 
 import networkx
 
+from utils import parse_interactome, scores_to_TSV
 
 # set up logger, using inherited config, in case we get called as a module
 logger = logging.getLogger(__name__)
-
-
-def parse_interactome(interactome_file) -> tuple[networkx.Graph, dict]:
-    '''
-    Creates a networkx.Graph interactome.
-
-    arguments:
-    - interactome_file: path to interactome SIF file, type=pathlib.Path
-      with 3 columns: gene1 pp gene2
-    
-    returns:
-    - interactome: type=networkx.Graph
-    - genes: dict with key=gene value=0
-    '''
-    interactome = networkx.Graph()
-    genes = {}
-
-    try:
-        f = open(interactome_file, 'r')
-    except Exception as e:
-        logger.error("Opening provided SIF interactome file %s: %s", interactome_file, e)
-        raise Exception("cannot open provided interactome file")
-
-    for line in f:
-        line_splitted = line.rstrip().split('\t')
-        if len(line_splitted) != 3:
-            logger.error("SIF file %s has bad line (not 3 tab-separated fields): %s", interactome_file, line)
-            raise Exception("Bad line in the interactome file")
-
-        gene1, pp, gene2 = line_splitted
-
-        # exclude self-interactions
-        if gene1 == gene2:
-            continue
-        # else: ppopulate structures
-        interactome.add_edge(gene1, gene2)
-        genes[gene1] = 0
-        genes[gene2] = 0
-
-    return (interactome, genes)
 
 
 def calculate_degree_centrality(interactome) -> dict:
@@ -67,26 +28,6 @@ def calculate_degree_centrality(interactome) -> dict:
     return scores
 
 
-def scores_to_TSV(scores, out_path):
-    '''
-    Save scoring results to a TSV file with 2 columns: gene, score.
-
-    arguments:
-    - scores: dict with key=gene, value=score
-    - out_path: path to save TSV, type=pathlib.Path
-    '''
-    out_file = out_path / "degree_centrality.tsv"
-    f = open(out_file, 'w+')
-
-    # file header
-    f.write("node\t" + "DC" + '\n')
-
-    for node, score in scores.items():
-        f.write(str(node) + '\t' + str(score) + '\n')
-
-    f.close()
-
-
 def main(interactome_file, out_path):
 
     logger.info("Parsing interactome")
@@ -96,7 +37,7 @@ def main(interactome_file, out_path):
     degree_centrality = calculate_degree_centrality(interactome)
 
     logger.info("Done!")
-    scores_to_TSV(degree_centrality, out_path)
+    scores_to_TSV(degree_centrality, out_path, file_name="scores_DC.tsv")
 
 
 if __name__ == "__main__":
