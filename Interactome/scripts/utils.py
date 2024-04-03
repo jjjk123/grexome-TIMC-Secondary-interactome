005c6c2d586f3f39fc8cf3c796635d56116032e7
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2024-04-03 14:08:33 +0200

    utils.py : parse_causal_genes code review

--- Interactome/scripts/utils.py
+++ Interactome/scripts/utils.py
@@ -28,10 +28,11 @@ def parse_interactome(interactome_file) -> networkx.Graph:
     for line in f:
         split_line = line.rstrip().split('\t')
         if len(split_line) != 3:
-            logger.error("SIF file %s has bad line (not 3 tab-separated fields): %s", interactome_file, line)
+            logger.error("SIF file %s has bad line (not 3 tab-separated fields): %s",
+                         interactome_file, line)
             raise Exception("Bad line in the interactome file")
 
-        gene1, pp, gene2 = line_splitted
+        gene1, pp, gene2 = split_line
 
         # exclude self-interactions
         if gene1 == gene2:
@@ -42,69 +43,62 @@ def parse_interactome(interactome_file) -> networkx.Graph:
     return (interactome)
 
 
-def parse_causal_genes(causal_genes_file, gene2ENSG_file, genes) -> dict:
+def parse_causal_genes(causal_genes_file, gene2ENSG_file, patho="MMAF") -> dict:
     '''
-    Creates a dictionary of all genes in the interactome, key=ENSG, value=1 if causal, 0 otherwise.
+    Build a dict of causal ENSGs for patho
 
     arguments:
-    - causal_genes_file: path to known causal genes CSV file, type=str
-      with 2 columns: gene, pathology
-    - canonical_genes_file: path to canonical genes, type=str with 2
-      columns: gene_name, ENSG
-    - genes: dict with key=gene, value=0
-    NOTE: pathology of interest is hardcoded
+    - causal_genes_file: filename (with path) of known causal genes TSV file
+      with 2 columns: gene_name pathologyID, type=str
+    - gene2ENSG_file: filename (with path) of TSV file mapping gene names to ENSGs, 
+      type=str with 2 columns: gene_name, ENSG
+    - pathologyID of interest, causal genes for other pathologyIDs are ignored
 
     returns:
-    - causal_genes: dict with key=ENSG, value=1 if causal, 0 otherwise
+    - causal_genes: dict of all causal genes with key=ENSG, value=1
     '''
-    causal_genes = genes.copy()
-    canonical_genes = {}
+    causal_genes = {}
 
-    # first, parse canonical genes
+    # populate gene2ENSG: key=gene_name, value=ENSG
+    gene2ENSG = {}
     try:
-        f_canonical = open(canonical_genes_file, 'r')
+        f_gene2ENSG = open(gene2ENSG_file, 'r')
     except Exception as e:
-        logger.error("Opening provided canonical genes file %s: %s", canonical_genes_file, e)
-        raise Exception("cannot open provided canonical genes file")
+        logger.error("Opening provided gene2ENSG file %s: %s", gene2ENSG_file, e)
+        raise Exception("cannot open provided gene2ENSG file")
 
-    # skip header
-    next(f_canonical)
+    # not sure if there's a header line -> just put everything in gene2ENSG
+    for line in f_gene2ENSG:
+        split_line = line.rstrip().split('\t')
+        if len(split_line) != 2:
+            logger.error("gene2ENSG file %s has bad line (not 2 tab-separated fields): %s",
+                         gene2ENSG_file, line)
+            raise Exception("Bad line in the gene2ENSG file")
+        gene_name, ENSG = split_line
+        gene2ENSG[gene_name] = ENSG
 
-    for line in f_canonical:
-        line_splitted = line.rstrip().split('\t')
+    f_gene2ENSG.close()
 
-        gene_name, ENSG = line_splitted
-
-        # populate canonical genes dictionary
-        canonical_genes[gene_name] = ENSG
-
-    f_canonical.close()
-
-    # second, parse causal genes
+    # parse causal genes
     try:
         f_causal = open(causal_genes_file, 'r')
     except Exception as e:
         logger.error("Opening provided causal genes file %s: %s", causal_genes_file, e)
         raise Exception("cannot open provided causal genes file")
 
-    # doesn't this file have a header? (should be documented in top-of-function comments)
+    # not sure if there's a header line, assume there isn't
     for line in f_causal:
-        line_splitted = line.rstrip().split('\t')
+        split_line = line.rstrip().split('\t')
+        if len(split_line) != 2:
+            logger.error("causal_genes file %s has bad line (not 2 tab-separated fields): %s",
+                         causal_genes_file, line)
+            raise Exception("Bad line in the causal_genes file")
+        gene_name, pathology = split_line
 
-        gene_name, pathology = line_splitted
-
-        # map gene names to ENSGs
-        if gene_name in canonical_genes:
-            ENSG = canonical_genes[gene_name]
-        else:
+        if pathology != patho:
             continue
-
-        # skip genes not present in the interactome
-        if ENSG not in genes:
-            continue
-
-        # populate structures
-        if pathology == "MMAF":
+        elif gene_name in gene2ENSG:
+            ENSG = gene2ENSG[gene_name]
             causal_genes[ENSG] = 1
 
     f_causal.close()
